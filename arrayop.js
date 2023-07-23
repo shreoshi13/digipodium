c6b1881699a4d16bdf9add961d1317b58fa36993
shreoshi roy <shreoshi63@gmail.com>
2023-07-23 10:33:51 +0530

    changes

--- arrayop.js
+++ arrayop.js
@@ -20,3 +20,8 @@ const price = [651, 98,20,666,999]
 //use map to get discounted prices at 7.5%
 const result3 = price.map((p)=>{return p-0.075*p}) 
 console.log(result3)
+
+const prices=['$67.89','$348.99','$29384.22','$4.102', '$1.241']
+//use map to get the prices in integer format
+const result4=prices.map((w)=>{return parseInt(w.slice(1))})
+console.log(result4)
