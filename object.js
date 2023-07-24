880c7b2cca4c1163281103a8c953d50042815056
shreoshi roy <shreoshi63@gmail.com>
2023-07-24 10:46:06 +0530

    changes

---
+++ object.js
@@ -0,0 +1,6 @@
+user = { name: 'shreoshi' , email : 'shreoshi@gmail.com', password : '768'};
+console.log(user.name)
+console.log(user.email)
+console.log(user['email'])
+user.address = 'lucknow'
+console.log(user)
