6675d47fcbb4fbbc7f0a2dac87f5553b6ac3815b
shreoshi roy <shreoshi63@gmail.com>
2023-07-24 10:58:19 +0530

    changes

--- object.js
+++ object.js
@@ -3,6 +3,17 @@ console.log(user.name)
 console.log(user.email)
 console.log(user['email'])
 user.address = 'lucknow'
+user.clg = 'srm'
 console.log(user)
 let customkey = 'password'
 console.log(user[customkey])
+let smartphone ={
+    brand : 'samsung',
+    model : 's21',
+    color : 'black',
+    price : '30000'
+};
+smartphone.color = [ 'blue','red','grey']
+smartphone.color.push('silver')
+console.log(smartphone.color[2])
+console.log(smartphone.color)
