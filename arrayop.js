74907dbf55de9bc60250d4703ae01f02680b7c6b
shreoshi roy <shreoshi63@gmail.com>
2023-07-24 09:58:04 +0530

    changes

--- arrayop.js
+++ arrayop.js
@@ -25,3 +25,22 @@ const prices=['$67.89','$348.99','$29384.22','$4.102', '$1.241']
 //use map to get the prices in integer format
 const result4=prices.map((w)=>{return parseInt(w.slice(1))})
 console.log(result4)
+
+const names = ["jay raj", "ali khan", "bobby roy", "riya chudail"];
+
+const namesInUpperCase = names.map((name) => name.toUpperCase());
+
+console.log(namesInUpperCase);
+
+const numsat=[2,4,6,8]
+const resultnew = numsat.map( (n) => {return n**2} );
+console.log(resultnew)
+
+const numbers = [144, 625, 25, 49, 121];
+
+const squareRoots = numbers.map((number) => Math.sqrt(number));
+
+console.log(squareRoots);
+
+//use map function to extract first names from the array of full names
+console.log(names.map((name)=>{return name.split(' ')[0]}))
