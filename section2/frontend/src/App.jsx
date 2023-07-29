6120250ecd3d4fd0769e64e1a305381a5610905d
shreoshi roy <shreoshi63@gmail.com>
2023-07-29 10:28:43 +0530

    changes

--- section2/frontend/src/App.jsx
+++ section2/frontend/src/App.jsx
@@ -3,20 +3,24 @@ import './App.css';
 import Home from './components/Home';
 import About from './components/about';
 import Login from './components/Login';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div >
-      <h1 className='myclass'>
-        my react app
-      </h1>
-      <h2 style={{color: 'green', fontSize: 50}}>
-        using inline css
-      </h2>
-      <input type='text' />
-      <Home />
-      <About />
-      <Login />
+     <BrowserRouter>
+        <Link to="/home">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/login">Login</Link>
+
+     <Routes>
+      <Route element={ <Home /> } path='home'/>
+      <Route element={ <About /> } path='about'/>
+      <Route element={ <Login /> } path='login'/>
+      <Route element={ <Home /> } path='/'/>
+     </Routes>
+     </BrowserRouter>
+      
     </div>
   );
 }
--- section2/frontend/src/components/Home.jsx
+++ section2/frontend/src/components/Home.jsx
@@ -6,6 +6,13 @@ const Home = () => {
             <h1>
                 welcome to homepage
             </h1>
+            <h1 className='myclass'>
+        my react app
+      </h1>
+      <h2 style={{color: 'green', fontSize: 50}}>
+        using inline css
+      </h2>
+      <input type='text' />
         </div>
     )
 }
