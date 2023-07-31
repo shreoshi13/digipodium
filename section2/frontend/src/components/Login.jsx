f80706d73f7167cf225e32c1f42f065bd00ea6d2
shreoshi roy <shreoshi63@gmail.com>
2023-07-31 13:48:24 +0530

    changes

--- section2/frontend/src/components/Login.jsx
+++ section2/frontend/src/components/Login.jsx
@@ -1,12 +1,47 @@
 import React from 'react'
 const Login = () => {
     return (
-        <div>
+            <div>
             <h2>
-                this is third
+                login page
             </h2>
-        </div>
-    )
-}
+    <div className='body' style={{ width : '100%', height : '90vh',backgroundColor : 'lightPink'}}>
+      <div className = 'row' style = {{display : 'flex', justifyContent : 'space-evenly'}}>
+        <div className = 'col-1' style = {{backgroundColor : 'ButtonShadow', width : '40vw', height : '70vh',
+         marginTop : '80px', borderRadius : '40px', border: '10px solid lightGrey'}} >
+            <h3 style = {{textAlign : 'center', fontFamily : 'fantasy', paddingTop : '30px'}}>Sign Up</h3>
+             <h5 style={{marginTop : '30px'}}>Name:</h5>
+             <input type="text" style={{width : '80%'}}/>
+             <h5>Mobile No:</h5>
+             <input type="number" style={{width : '80%'}} />
+             <h5>Email Id:</h5>
+             <input type="email" style={{width : '80%'}}  />
+             <h5>Password:</h5>
+             <input type="password" style={{width : '80%'}} /> 
+             <br />
+             <button style = {{marginTop : '35px', width : '180px', height: '45px', marginLeft : '160px',
+              backgroundColor : 'lightgreen', border: 'none', borderRadius: '15px', fontWeight: 'bold'}}>Sign UP</button>
 
+        </div>
+        <div className = 'col-2' style = {{backgroundColor : 'ButtonShadow', width : '40vw', height : '70vh',
+         marginTop : '80px', borderRadius : '40px', border: '10px solid lightGrey'}}>
+        <h3 style = {{textAlign : 'center', fontFamily : 'fantasy', paddingTop : '30px'}}>Login</h3>
+             <h5 style={{marginTop : '30px'}}>User Name:</h5>
+             <input type="text" style={{width : '80%'}}/>
+             <h5>Email Id:</h5>
+             <input type="email" style={{width : '80%'}} />
+             <h5>Password:</h5>
+             <input type="password" style={{width : '80%'}}  />
+             <h5>Confirm Password:</h5>
+             <input type="password" style={{width : '80%'}} /> 
+             <br />
+             <button style = {{marginTop : '35px', width : '180px', height: '45px', marginLeft : '160px',
+              backgroundColor : 'lightblue', border: 'none', borderRadius: '15px', fontWeight: 'bold'}}>Login</button>
+
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+  }
 export default Login;
--- section2/frontend/src/components/about.jsx
+++ section2/frontend/src/components/about.jsx
@@ -3,10 +3,39 @@ const About = () => {
     return(
         <div>
             <h2>
-                this is second page
+                about page
             </h2>
-        </div>
-    )
+          
+    <div className='body' style = {{backgroundColor : 'grey'}}>
+    <div style = {{ width : '100%', height : '60px', backgroundColor : 'black'}}>
+    <h2 style = {{color : 'white', textAlign : 'center', paddingTop : '10px', fontFamily : 'serif'}}>ABOUT US</h2>
+    </div>
+     <div className='container' style = {{width : '200vw', height : '82.3vh', backgroundColor : 'grey',position : 'relative'}} >
+       <div className='row' style={{display : 'flex', justifyContent : 'space-around',alignItems : 'center', paddingTop : '100px'}}>  
+        <div className='col-1' style = {{backgroundColor : 'black', color : 'white', width : '300px', height : '300px', textAlign : 'center', borderRadius : '25px'}}>
+          <h4 style={{fontFamily : 'serif'}}>VISION</h4>
+          <p style={{paddingTop : '25px'}}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur, fugiat. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+             Consequuntur, fugiat
+             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur, fugiat</p>
+         </div>
+         <div className='col-2' style = {{backgroundColor : 'black',color : 'white', width : '300px', height : '300px', textAlign : 'center',borderRadius : '25px' }}>
+          <h4 style={{fontFamily : 'serif'}}>MISSION</h4>
+          <p style={{paddingTop : '25px'}}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur, fugiat. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+             Consequuntur, fugiat
+           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur, fugiat</p>
+         </div>
+         <div className='col-3' style = {{backgroundColor : 'black',color : 'white', width : '300px', height : '300px', textAlign : 'center',borderRadius : '25px' }}>
+          <h4 style={{fontFamily : 'serif'}}>VALUES</h4>
+          <p style={{paddingTop : '25px'}}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur, fugiat.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur, fugiat
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur, fugiat
+          </p>
+         </div>
+         
+       </div>
+    </div>
+    </div>
+    </div>
+  )
 }
-
 export default About;
