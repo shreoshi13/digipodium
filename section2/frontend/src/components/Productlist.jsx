2d65fb6a6d6cf816bdb219011e3acc2ea01a7925
shreoshi roy <shreoshi63@gmail.com>
2023-08-06 10:36:27 +0530

    changes

--- section2/frontend/src/components/Productlist.jsx
+++ section2/frontend/src/components/Productlist.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
 import smartphones from './dummyData';
+const brands=['samsung','apple','oppo','vivo']
 
 const Productlist = () => {
     const [productData, setproductData] = useState(smartphones);
     const displayProducts = ()=>{
         return productData.map((phone)=>{
-            return <div className='col-md-3'>
+            return <div className='col-md-3 mb-4'>
                 <div className='card'>
-                    <img className='card-img-top' src={phone.image} alt="" />
+                    <img style={{height: '200px', objectFit: 'cover' }} className='card-img-top' src={phone.image} alt="" />
                     <div className='card-body'>
                         <h5>
                             {phone.brand}
@@ -22,18 +23,52 @@ const Productlist = () => {
                 </div>
             </div>
         })
+    };
+    const searchProduct = (e)=>{
+        const search = e.target.value;
+        setproductData(
+            smartphones.filter ((phone)=>{
+                return phone.brand.toLowerCase().includes(search.toLowerCase())
+            })
+        );
+    };
+    const filterBrands = (e)=>{
+        const search=e.target.value;
+        setproductData(
+            smartphones.filter((phone)=>{
+                return phone.brand===search;
+            })
+        )
     }
   return (
     <div>
-        <div className='container'>
-            <div className='row'>
+        <header className='bg-body-secondary'>
+        <div className='container py-5'>
+            <h1 className='text-center'>product list page</h1>
+
+            <input onChange={searchProduct} type='text' className='form-control form-control-lg' placeholder='search product name' />
+           <select onChange={filterBrands} name="" id="">
+            {
+                brands.map((brand) => {
+                    return <option value={brand}>
+                        {brand}
+                    </option>
+                })
+            }
+           </select>
+            </div>
+
+            </header>
+            <div className='container'>
+            <div className='row mt-4'>
                 {
                     displayProducts()
                 }
 
             </div>
         </div>
-    </div>
+        </div>
+
   )
 }
 
