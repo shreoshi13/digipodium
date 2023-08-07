849ce3b89ce297cb7a5a76818dfc0cced8338f60
shreoshi roy <shreoshi63@gmail.com>
2023-08-07 11:05:07 +0530

    changes

--- section2/frontend/src/components/Login.jsx
+++ section2/frontend/src/components/Login.jsx
@@ -4,7 +4,12 @@ const Login = () => {
   //initializing formik
   const loginForm = useFormik({
     initialValues:{
-      email:""
+      email:"",
+      password:""
+    },
+    onSubmit:(values)=>{
+        console.log(values);
+        //code to submit form to server
     }
   });
     return (
@@ -17,15 +22,15 @@ const Login = () => {
         <div className = 'col-1' style = {{backgroundColor : 'ButtonShadow', width : '40vw', height : '70vh',
          marginTop : '80px', borderRadius : '40px', border: '10px solid lightGrey'}} >
             <h3 style = {{textAlign : 'center', fontFamily : 'fantasy', paddingTop : '30px'}}>Sign Up</h3>
-            <form action="">
+            <form onSubmit={loginForm.handleSubmit}>
              <h5 style={{marginTop : '30px'}}>Name:</h5>
              <input type="text" style={{width : '80%'}}/>
              <h5>Mobile No:</h5>
              <input type="number" style={{width : '80%'}} />
              <h5>Email Id:</h5>
-             <input type="email" style={{width : '80%'}}  />
+             <input type="email" name='email' onChange={loginForm.handleChange} value={loginForm.values.email} style={{width : '80%'}}  />
              <h5>Password:</h5>
-             <input type="password" style={{width : '80%'}} /> 
+             <input type="password" name='password' onChange={loginForm.handleChange} value={loginForm.values.password} style={{width : '80%'}} /> 
              <br />
              <button style = {{marginTop : '35px', width : '180px', height: '45px', marginLeft : '160px',
               backgroundColor : 'lightgreen', border: '1px', borderRadius: '15px', fontWeight: 'bold'}}>Sign UP</button>
@@ -34,7 +39,7 @@ const Login = () => {
         <div className = 'col-2' style = {{backgroundColor : 'ButtonShadow', width : '40vw', height : '70vh',
          marginTop : '80px', borderRadius : '40px', border: '10px solid lightGrey'}}>
         <h3 style = {{textAlign : 'center', fontFamily : 'fantasy', paddingTop : '30px'}}>Login</h3>
-        <form action="">
+        <form onChange={loginForm.handleSubmit}>
              <h5 style={{marginTop : '30px'}}>User Name:</h5>
              <input type="text" style={{width : '80%'}}/>
              <h5>Email Id:</h5>
