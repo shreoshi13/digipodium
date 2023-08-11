8cd744de8658bb2aad1ffc7bd02e7244fa7b1cc9
shreoshi roy <shreoshi63@gmail.com>
2023-08-11 10:24:54 +0530

    changes

--- section3/backend/index.js
+++ section3/backend/index.js
@@ -5,4 +5,22 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+//routes
+app.get('/', (request,response)=>{
+    response.send('response from express');
+});
+
+//add
+app.get('/add',(req,res)=>{
+    res.send('add the data')
+});
+//getall
+app.get('/getall',(req,res)=>{
+    res.send('get the data')
+});
+//delete
+app.get('/delete',(req,res)=>{
+    res.send('delete the data')
+});
+
 app.listen(port, ()=>{console.log('express server started at 5000')});
