f5ce63c2ece5cfb40eed169446ac889e6b6b4b0d
shreoshi roy <shreoshi63@gmail.com>
2023-08-12 10:10:20 +0530

    changes

---
+++ section3/backend/connection.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+const url = "mongodb+srv://shreoshi63:Shreoshi@cluster0.8wmjrz3.mongodb.net/mydatabase?retryWrites=true&w=majority";
+mongoose.connect(url);
+
