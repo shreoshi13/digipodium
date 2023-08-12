d5cee7d5069c6136104ca2bd2232cdb5938df7bf
shreoshi roy <shreoshi63@gmail.com>
2023-08-12 10:20:45 +0530

    changes

--- section3/backend/connection.js
+++ section3/backend/connection.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 
 const url = "mongodb+srv://shreoshi63:Shreoshi@cluster0.8wmjrz3.mongodb.net/mydatabase?retryWrites=true&w=majority";
-mongoose.connect(url);
+
+//asynchronous function returns a promise
+mongoose.connect(url)
+.then((result) => {
+    console.log('database connected');
+}).catch((err) => {
+    console.log(err);
+});
 
