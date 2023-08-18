7d4c76ee99d132f17b178a8a446da32f8928f548
shreoshi roy <shreoshi63@gmail.com>
2023-08-18 20:52:06 +0530

    changes

--- section3/backend/routers/userRouter.js
+++ section3/backend/routers/userRouter.js
@@ -7,7 +7,7 @@ router.post('/add',(req,res)=>{
     console.log(req.body);
     new Model(req.body).save()
     .then((result) => {
-        res.json(result);
+        setTimeout( () => { res.json(result); }, 3000 );
     })
     .catch((err) => {
         console.log(err);
@@ -72,5 +72,17 @@ router.put('/update/:id',(req,res)=>{
     });
 });
 
+router.post('/authenticate', (req, res) => {
+    console.log(req.body);
+    Model.findOne(req.body)
+    .then((result) => {
+        if(result !== null) res.json(result);
+        else res.status(401).json({message : 'login failed'})
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports=router;
